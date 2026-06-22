2af1588bc6c0ba310b8c7adc60644d28c9b8bbcc
“sohamadhikari2000” <sohamadhikari2000@gmail.com>
2026-06-22 14:05:55 +0200

    deployment configuration

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import pandas as pd
 from src.exception import CustomException
 
 import dill #can us e pickle also
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
 
@@ -55,3 +56,11 @@ def evaluate_model(X_train,y_train,X_test,y_test,models,params):
     except Exception as e:
         raise CustomException(e,sys)
 
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return pickle.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e,sys)
+    
