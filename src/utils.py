7fd72724af5c748d94ba28e08758f245790284d4
“sohamadhikari2000” <sohamadhikari2000@gmail.com>
2026-06-24 03:49:06 +0200

    removed hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -22,24 +22,24 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_model(X_train,y_train,X_test,y_test,models,params):
+def evaluate_model(X_train,y_train,X_test,y_test,models):
     try:
         report = {}
         for i in range(len(models)):
             model = list(models.values())[i]
-            param = params[list(models.keys())[i]]
+            # param = params[list(models.keys())[i]]
 
-            gs = GridSearchCV(
-                estimator=model,
-                param_grid=param,
-                cv=3
-            )
+            # gs = GridSearchCV(
+            #     estimator=model,
+            #     param_grid=param,
+            #     cv=3
+            # )
 
             # model.fit(X_train,y_train)
             
-            gs.fit(X_train,y_train)
+            # gs.fit(X_train,y_train)
 
-            model.set_params(**gs.best_params_)
+            # model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
@@ -55,6 +55,42 @@ def evaluate_model(X_train,y_train,X_test,y_test,models,params):
         return report
     except Exception as e:
         raise CustomException(e,sys)
+    
+   
+# def evaluate_model(X_train,y_train,X_test,y_test,models,params):
+#     try:
+#         report = {}
+#         for i in range(len(models)):
+#             model = list(models.values())[i]
+#             param = params[list(models.keys())[i]]
+
+#             gs = GridSearchCV(
+#                 estimator=model,
+#                 param_grid=param,
+#                 cv=3
+#             )
+
+#             # model.fit(X_train,y_train)
+            
+#             gs.fit(X_train,y_train)
+
+#             model.set_params(**gs.best_params_)
+#             model.fit(X_train,y_train)
+
+#             y_train_pred = model.predict(X_train)
+#             y_test_pred = model.predict(X_test)
+
+#             train_model_score = r2_score(y_train,y_train_pred)
+#             test_model_score = r2_score(y_test,y_test_pred)
+
+#             # print(list(models.keys())[i])
+
+#             report[list(models.keys())[i]] = test_model_score
+
+#         return report
+#     except Exception as e:
+#         raise CustomException(e,sys)
+
 
 def load_object(file_path):
     try:
