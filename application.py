ac2a5df4753c59b3cb13c213e26c00a194c15d8d
“sohamadhikari2000” <sohamadhikari2000@gmail.com>
2026-06-24 04:01:49 +0200

    fix something

--- application.py
+++ application.py
@@ -6,14 +6,12 @@ from src.pipeline.predict_pipeline import PredictPipeline, CustomData
 
 application = Flask(__name__)
 
-app = application
 
-
-@app.route('/')
+@application.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/predictdata',methods=["GET","POST"])
+@application.route('/predictdata',methods=["GET","POST"])
 def predict_datapoint():
     if request.method == "GET":
         return render_template("home.html")
@@ -37,4 +35,4 @@ def predict_datapoint():
     
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    application.run(host="0.0.0.0")
